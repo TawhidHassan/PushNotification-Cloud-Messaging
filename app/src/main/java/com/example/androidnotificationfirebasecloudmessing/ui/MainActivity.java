dc2805dcae9f76670c4f377b33f84c88449bf746
sifat hassan <sifat007.sh7@gmail.com>
2021-06-12 19:58:10 +0600

    user login&sigup nad save user data realtime database

---
+++ app/src/main/java/com/example/androidnotificationfirebasecloudmessing/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidnotificationfirebasecloudmessing.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.androidnotificationfirebasecloudmessing.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
