3845a58f65d6b98a612d5438fbdd98d110648820
sifat hassan <sifat007.sh7@gmail.com>
2021-06-12 20:09:17 +0600

    BottomNavigation and funtionality done

--- app/src/main/java/com/example/androidnotificationfirebasecloudmessing/ui/MainActivity.java
+++ app/src/main/java/com/example/androidnotificationfirebasecloudmessing/ui/MainActivity.java
@@ -1,18 +1,63 @@
 package com.example.androidnotificationfirebasecloudmessing.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androidnotificationfirebasecloudmessing.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView navigationView;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.navigation);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Notification");
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.all_user:
+                        replace(new AllUserFragment());
+                        break;
+                    case R.id.topic:
+                        replace(new TopicFragment());
+                        break;
+                    case R.id.profile:
+                        replace(new ProfileFragment());
+                        break;
+                    case R.id.notification:
+                        replace(new NotificationFragment());
+                        break;
+
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void replace(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
     }
 }
